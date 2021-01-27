869caa7a17b69d33945c629a0760f719eb5c08c8
Neema Nejad <ngnejad@ucdavis.edu>
2021-01-27 15:55:23 -0800

    added read_record function in page set class
    rids, schema encodings, indirections, and timestamps are now set in add_record for the page range class

--- template/pageRange.py
+++ template/pageRange.py
@@ -1,5 +1,6 @@
 from template.pageSet import PageSet
 from template.config import *
+import time
 
 
 class PageRange:
@@ -9,22 +10,37 @@ class PageRange:
         self.num_records = 0
         self.base_page_sets = []
         self.tail_page_sets = []
-        self.rids = {}
+        self.rids = {}  # maps rids to (base_page_set index, page offset)
         self.schema_encodings = []
         self.indirections = []
         self.timestamps = []
-        self.init_base_page_sets()
+        self.__init_base_page_sets()
 
-    def init_base_page_sets(self):
+    def __init_base_page_sets(self):
         for i in range(PAGE_SETS):
             self.base_page_sets.append(PageSet(self.num_columns))
 
-    def add_tail_page_set(self):
+    def __add_tail_page_set(self):
         self.tail_page_sets.append(PageSet(self.num_columns))
 
     def add_record(self, *columns):
+        # add key-value pair to rids where key is the rid and the value is a tuple containing the
+        # base_page_set index and record offset
+        self.rids[START_RID + self.num_records] = (self.num_columns / RECORDS_PER_PAGE, self.num_records)
+
+        # add record to page set
         self.base_page_sets[self.num_records / RECORDS_PER_PAGE].add_record(columns)
+
+        # add appropriate schema encoding, indirection, and timestamp
+        self.schema_encodings[self.num_records] = '0' * self.num_columns
+        self.indirections[self.num_records] = None
+        self.timestamps[self.num_records] = time.strftime("%H:%M", time.localtime())
+
         self.num_records += 1
+
+    def get_record(self, rid, query_columns):
+        page_set_index, offset = self.rids[rid]
+        return self.base_page_sets[page_set_index].read_record(offset, query_columns)
 
     def remove_record(self):
         pass
--- template/pageSet.py
+++ template/pageSet.py
@@ -7,12 +7,22 @@ class PageSet:
     def __init__(self, num_columns):
         self.num_columns = num_columns
         self.pages = []
-        self.create_pages()
+        self.__create_pages()
 
-    def create_pages(self):
+    def __create_pages(self):
         for i in range(self.num_columns):
             self.pages.append(Page())
 
     def add_record(self, *columns):
         for i in range(self.num_columns):
             self.pages[i].write(columns[i])
+
+    def read_record(self, offset, query_columns):
+        data = []
+        for i in range(len(query_columns)):
+            if query_columns[i] == 0:
+                continue
+
+            data.append(self.pages[i].read(offset))
+
+        return data
