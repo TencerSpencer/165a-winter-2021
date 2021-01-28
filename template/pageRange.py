b3817725c3a0aee4963aeef07f148278ecb370f0
Neema Nejad <ngnejad@ucdavis.edu>
2021-01-27 17:21:49 -0800

    add_record in page range class returns an rid and base page set index so the table class can assign keys to (rid, page range index, base page set index) accordingly

--- template/pageRange.py
+++ template/pageRange.py
@@ -20,8 +20,11 @@ class PageRange:
 
     def add_record(self, *columns):
         # add record to appropriate page set
-        self.base_page_sets[self.num_records / RECORDS_PER_PAGE].write_record(START_RID + self.num_records, columns)
+        rid = self.create_rid()
+        page_set_index = self.get_next_free_base_page_set()
+        self.base_page_sets[page_set_index].write_record(rid, columns)
         self.num_records += 1
+        return rid, page_set_index
 
     def get_record(self, rid, page_set_index, query_columns):
         return self.base_page_sets[page_set_index].read_record(rid, query_columns)
@@ -31,3 +34,15 @@ class PageRange:
 
     def update_record(self):
         pass
+
+    def create_rid(self):
+        return START_RID + self.num_records
+
+    def get_next_free_base_page_set(self):
+        return self.num_records / RECORDS_PER_PAGE
+
+    def has_space(self):
+        if self.num_records < PAGE_SETS * RECORDS_PER_PAGE:
+            return True
+
+        return False
