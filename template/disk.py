248bf6fb7090418ffee2e17d5faba52626659a86
Neema Nejad <37365315+sharkcooper@users.noreply.github.com>
2021-02-19 01:23:45 -0800

    Update disk.py

--- template/disk.py
+++ template/disk.py
@@ -2,14 +2,14 @@ from template.config import *
 
 
 class Disk:
-    def __init__(self, file_name):
-        self.fn = file_name
-        self.base_fn = TABLE_BASE_PATH + file_name + ".base"
-        self.tail_fn = TABLE_BASE_PATH + file_name + ".tail"
-        self.info_fn = TABLE_BASE_PATH + file_name + ".info"
-        self.base_meta_fn = TABLE_BASE_PATH + file_name + ".bmeta"
-        self.tail_meta_fn = TABLE_BASE_PATH + file_name + ".tmeta"
-        self.page_directory_fn = TABLE_BASE_PATH + file_name + ".pd"
+    def __init__(self, table_name):
+        self.fn = table_name
+        self.base_fn = TABLE_BASE_PATH + table_name + ".base"
+        self.tail_fn = TABLE_BASE_PATH + table_name + ".tail"
+        self.info_fn = TABLE_BASE_PATH + table_name + ".info"
+        self.base_meta_fn = TABLE_BASE_PATH + table_name + ".bmeta"
+        self.tail_meta_fn = TABLE_BASE_PATH + table_name + ".tmeta"
+        self.page_directory_fn = TABLE_BASE_PATH + table_name + ".pd"
 
     # returns base page set with associated tail page sets
     def read(self, base_rid):
