ed2ee7157a76b7e3613c1a0495e6258e51b653af
Neema Nejad <ngnejad@ucdavis.edu>
2021-02-19 00:52:25 -0800

    made Disk object
    added some skeleton code to Disk object

--- template/config.py
+++ template/config.py
@@ -2,6 +2,7 @@ START_RID = 0
 PAGE_SETS = 16
 PAGE_SIZE = 4096
 RECORDS_PER_PAGE = PAGE_SIZE / 8
+TABLE_BASE_PATH = "./Tables/"
 
 def init():
     pass
---
+++ template/disk.py
@@ -0,0 +1,18 @@
+from template.config import *
+
+
+class Disk:
+    def __init__(self, file_name):
+        self.fn = file_name
+        self.base_fn = TABLE_BASE_PATH + file_name + ".base"
+        self.tail_fn = TABLE_BASE_PATH + file_name + ".tail"
+        self.info_fn = TABLE_BASE_PATH + file_name + ".info"
+        self.base_meta_fn = TABLE_BASE_PATH + file_name + ".bmeta"
+        self.tail_meta_fn = TABLE_BASE_PATH + file_name + ".tmeta"
+        self.page_directory_fn = TABLE_BASE_PATH + file_name + ".pd"
+
+    def read(self, base_rid):
+        pass
+
+    def write(self, base_rid):
+        pass
