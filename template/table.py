efe8b34c32cc86ddb9e13fdee2893b264e842634
Spencer <sbthomas@ucdavis.edu>
2021-02-19 19:28:41 -0800

    mapping out merge

--- template/table.py
+++ template/table.py
@@ -32,9 +32,12 @@ class Table:
         
         # setup merge variables
         self.next_time_to_call = time.time() # prepare consistent time callback
+        # initially, call the thread instantly
         self.thread = self.thread = threading.Timer(self.next_time_to_call - time.time(), self.__merge_callback)
         self.thread.start()
         self.timer_interval = 0.5 # every half a second, the timer will trigger, used as an example
+
+        self.rids_to_merge = set()
 
 
     # for help with background process operation and to ensure timer is consistent
@@ -42,13 +45,34 @@ class Table:
     def __merge_callback(self): 
         
         print(datetime.datetime.now())
-        self.next_time_to_call = self.next_time_to_call + self.timer_interval
         # call _merge
+        self.__merge()
         self.thread = threading.Timer(self.next_time_to_call - time.time(), self.__merge_callback)
+        self.next_time_to_call = self.next_time_to_call + self.timer_interval
         self.thread.start()
 
     def __merge(self):
-        pass
+        if len(self.rids_to_merge) == 0:
+            print("no merge necessary, table is up to date")
+            # no other action needed here
+        else:
+            # here, we can do length or duration checks
+            # if we're doing a duration check, we need to check how much time has passed
+            print("merge necessary")
+
+            # steps to merge if merge is needed
+            # 1. bring original base page to mem and copy it over
+            # 2. make a copy of the original base record to ensure mutual exclusion
+            # 3. get the most up to date tail for the base record and put it in mem
+            # 4. append data from tail to base record
+            # 5. allocate space to new base record on page with available space
+            # 6. do we leave the rest of the tail untouched and just deallocate it from memory?
+            
+            # 7. keep original base page in mem/intact until no queries are being ran on it i.e. update/select
+            # 8. once original is no longer needed, update page directory to point to newe base record
+            # 9. do we deallocate the original base page here? -> free up the storage?
+            # or, do we assign a new RID to the new base, and free up the RID of the previous?
+            # 10. do this for each baseRID in the set
 
     def shut_down_timer(self):
         self.thread.cancel() # use this to halt timer
@@ -77,6 +101,10 @@ class Table:
         base_rid = self.keys[key]
         page_range_index = self.page_directory[base_rid][0]
         tail_rid = self.__get_next_tail_rid()
+        
+        # append to base RID to a set of RIDs to merge
+        self.rids_to_merge.add(base_rid)
+
         return self.page_range_array[page_range_index].update_record(base_rid, tail_rid, columns)
 
     def select_record(self, key, query_columns):
