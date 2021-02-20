28e1272732b3f9b14cd742fc555cfb16c2aefb6d
Spencer <sbthomas@ucdavis.edu>
2021-02-19 20:16:14 -0800

    still need to configure threading but will run until merge is complete

--- template/table.py
+++ template/table.py
@@ -1,7 +1,7 @@
 from template.index import Index
 from template.pageRange import PageRange
 from template.config import *
-import datetime, threading, time
+import datetime, threading, time, copy
 
 
 class Record:
@@ -32,26 +32,40 @@ class Table:
         
         # setup merge variables
         self.next_time_to_call = time.time() # prepare consistent time callback
+        self.timer_interval = 0.5 # every half a second, the timer will trigger, used as an example
+        self.thread_in_crit_section = False
+        self.thread_stopped = False
+        self.rids_to_merge = set()
         # initially, call the thread instantly
         self.thread = self.thread = threading.Timer(self.next_time_to_call - time.time(), self.__merge_callback)
         self.thread.start()
-        self.timer_interval = 0.5 # every half a second, the timer will trigger, used as an example
-
-        self.rids_to_merge = set()
 
 
     # for help with background process operation and to ensure timer is consistent
     # https://stackoverflow.com/questions/8600161/executing-periodic-actions-in-python
     def __merge_callback(self): 
-        
-        print(datetime.datetime.now())
-        # call _merge
-        self.__merge()
-        self.thread = threading.Timer(self.next_time_to_call - time.time(), self.__merge_callback)
-        self.next_time_to_call = self.next_time_to_call + self.timer_interval
-        self.thread.start()
 
+        if self.thread_stopped == False:
+            print(datetime.datetime.now())
+            # call _merge
+            self.__merge()
+            self.thread_in_crit_section = False
+            self.thread = threading.Timer(self.next_time_to_call - time.time(), self.__merge_callback)
+            self.next_time_to_call = self.next_time_to_call + self.timer_interval
+            self.thread.start() 
+
+    def shut_down_timer(self): # if constant printing is occurring, this wont properly stop
+        if self.thread_in_crit_section:
+            # kill a certain way
+            # self.thread.join()
+            self.thread_stopped = True
+        else:
+            self.thread.cancel()
+             # use this to halt timer, but does not halt thread as per documentation
+
+    # this implementation will first be in mem
     def __merge(self):
+        self.thread_in_crit_section = True
         if len(self.rids_to_merge) == 0:
             print("no merge necessary, table is up to date")
             # no other action needed here
@@ -59,24 +73,29 @@ class Table:
             # here, we can do length or duration checks
             # if we're doing a duration check, we need to check how much time has passed
             print("merge necessary")
-
-            # steps to merge if merge is needed
-            # 1. bring original base page to mem and copy it over
-            # 2. make a copy of the original base record to ensure mutual exclusion
-            # 3. get the most up to date tail for the base record and put it in mem
-            # 4. append data from tail to base record
-            # 5. allocate space to new base record on page with available space
-            # 6. do we leave the rest of the tail untouched and just deallocate it from memory?
+                
+            # deep copy set then clear original to prevent contention
+            rid_set = copy.deepcopy(self.rids_to_merge)
+            self.rids_to_merge.clear()
+            for base_rid in rid_set:
+                print(base_rid)
+                       
+                # 1. bring original base page to mem and copy it over
             
-            # 7. keep original base page in mem/intact until no queries are being ran on it i.e. update/select
-            # 8. once original is no longer needed, update page directory to point to newe base record
-            # 9. do we deallocate the original base page here? -> free up the storage?
-            # or, do we assign a new RID to the new base, and free up the RID of the previous?
-            # 10. do this for each baseRID in the set
+                # base_and_tail = read(base_rid) # should return the base and tail 
 
-    def shut_down_timer(self):
-        self.thread.cancel() # use this to halt timer
-        pass
+                # 2. make a copy of the original base record to ensure mutual exclusion
+                # 3. get the most up to date tail for the base record and put it in mem
+                # 4. append data from tail to base record
+                # 5. allocate space to new base record on page with available space
+                # 6. do we leave the rest of the tail untouched and just deallocate it from memory?
+            
+                # some of these steps can be done in their own for loop to prevent lengthy block
+                # 7. keep original base page in mem/intact until no queries are being ran on it i.e. update/select
+                # 8. once original is no longer needed, update page directory to point to newe base record
+                # 9. do we deallocate the original base page here? -> free up the storage?
+                # or, do we assign a new RID to the new base, and free up the RID of the previous?
+                # 10. do this for each baseRID in the set
 
 
 
