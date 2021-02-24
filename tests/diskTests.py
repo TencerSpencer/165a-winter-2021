b65edf6d5d8f74c71a7c814ec3738627f81b7e3d
Hunter Kennedy <lordkennedy99@gmail.com>
2021-02-24 12:38:22 -0800

    Added tests

--- tests/diskTests.py
+++ tests/diskTests.py
@@ -1,5 +1,8 @@
+import sys
+sys.path.append('./')
 import unittest
 from template.db import *
+from template.query import *
 
 class DiskTests(unittest.TestCase):
     def test_create_new_table(self):
@@ -12,6 +15,25 @@ class DiskTests(unittest.TestCase):
         db = Database()
         db.open("./ECS165")
         table = db.get_table("Test")
+        db.close()
+
+    def test_write_to_test_table(self):
+        db = Database()
+        db.open("./ECS165")
+        table = db.get_table("Test")
+        query = Query(table)
+        for i in range(100):
+            query.insert(*[i, 1, 2, 3, 4])
+        db.close()
+
+    # select(key, column, query_columns)
+    def test_read_from_test_table(self):
+        db = Database()
+        db.open("./ECS165")
+        table = db.get_table("Test")
+        query = Query(table)
+        for i in range(100):
+            query.select(i, -1, [None, 1, None, None, None])
         db.close()
 
 
