fe6a86dcb6744b7282de91f4e68e532587bcb3d1
Spencer Thomas <sbthomas@ucdavis.edu>
2021-03-10 13:55:39 -0800

    added locks to pages

--- template/page.py
+++ template/page.py
@@ -1,16 +1,18 @@
 from template.page_config import *
-
+import threading
 
 class Page:
 
     def __init__(self):
         self.num_records = 0
         self.data = bytearray(PAGE_SIZE)
+        self.write_page_lock =  threading.Lock()
 
     def has_capacity(self):
         return self.num_records < RECORDS_PER_PAGE
 
     def write(self, value):
+        self.write_page_lock.acquire()
         value_in_bytes = []
         val = value
         for i in range(8):  # convert 64 bit value into byte sized chunks
@@ -20,8 +22,10 @@ class Page:
             self.data[(self.num_records * 8) + i] = value_in_bytes[i]
 
         self.num_records += 1
+        self.write_page_lock.release()
 
     def overwrite(self, value, offset):
+        self.write_page_lock.acquire()
         value_in_bytes = []
         val = value
         for i in range(8):  # convert 64 bit value into byte sized chunks
@@ -29,8 +33,12 @@ class Page:
 
         for i in range(8):  # add byte sized chunks to data
             self.data[(offset * 8) + i] = value_in_bytes[i]
+        self.write_page_lock.release()
 
     def read(self, offset):
+        self.write_page_lock.acquire()
         # convert bytes from data to a 64 bit integer and return value
-        return int.from_bytes(self.data[(offset * 8):(offset * 8) + 8], "little")
+        ret = int.from_bytes(self.data[(offset * 8):(offset * 8) + 8], "little")
+        self.write_page_lock.release()
+        return ret
 
